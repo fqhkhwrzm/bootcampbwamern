dd4518eb5f1970f673d4e8e495cbe056b45dde8f
fqhkhrzm <muhammadfaqihk16@gmail.com>
2025-04-14 23:11:58 +0700

    menambahkan mock dan memperbaiki router untuk talents dan events

--- app/api/v1/events/router.js
+++ app/api/v1/events/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express();
 const { create, index, find, destroy, update } = require('./controller');
+const { authenticateUser, authorizeRoles } = require('../../../middlewares/auth');
 
-router.get('/events', index);
-router.get('/events/:id', find);
-router.put('/events/:id', update);
-router.post('/events', create);
-router.delete('/events/:id', destroy);
+router.get('/events', authenticateUser, authorizeRoles('organizer'), index);
+router.get('/events/:id', authenticateUser, authorizeRoles('organizer'), find);
+router.put('/events/:id', authenticateUser, authorizeRoles('organizer'), update);
+router.post('/events', authenticateUser, authorizeRoles('organizer'), create);
+router.delete('/events/:id', authenticateUser, authorizeRoles('organizer'), destroy);
 
 module.exports = router;
